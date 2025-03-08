c53a59e7c9ac149994cb189a014e23dfe766b8c2
APavlov <sologod@yandex.ru>
2025-03-08 15:15:33 +0300

    go init

---
+++ backend/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// Подключение к MongoDB
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	_, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// tasksCollection := client.Database("todolist").Collection("tasks")
+	// userCollection := client.Database("todolist").Collection("users")
+
+	// Инициализация маршрутизатора
+	r := mux.NewRouter()
+	// handlers.InitTaskHandlers(tasksCollection)
+	// handlers.InitAuthHandlers(userCollection)
+
+	// Настройка CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
+		AllowedHeaders:   []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+
+	// Оберните маршрутизатор в CORS
+	handler := c.Handler(r)
+
+	// Запуск сервера
+	log.Println("Сервер запущен на http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
