ff01a288e22b9bc3ca5eb99143c28f3d0c4c1784
APavlov <sologod@yandex.ru>
2025-03-10 16:16:40 +0300

    model task

---
+++ backend/models/tasks.go
@@ -0,0 +1,10 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Task struct {
+	ID     primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	UserID primitive.ObjectID `json:"user_id" bson:"user_id"`
+	Title  string             `json:"title" bson:"title"`
+	Status string             `json:"status" bson:"status"`
+}
