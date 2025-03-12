6f1bf942ce2e3d58b48f14717c52944b20337ba1
APavlov <sologod@yandex.ru>
2025-03-12 17:30:41 +0300

    login form

--- frontend/src/Auth/Auth.js
+++ frontend/src/Auth/Auth.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Auth = () => (
-  <div className="flex justify-center items-center h-full">login</div>
+  <div className="flex justify-center items-center h-full">
+    <div className="h-80 w-72 bg-black/50 border-2 border-neutral-400 rounded">
+      <div className="text-primary flex justify-center pt-4 text-lg">
+        Todo List
+      </div>
+      <div className="flex flex-col gap-2 p-4">
+        <label htmlFor="email" className="text-white/50">
+          Email:
+        </label>
+        <input />
+        <label htmlFor="password" className="text-white/50">
+          Password:
+        </label>
+        <input />
+      </div>
+      <div className="flex justify-center pt-4">
+        <button className="text-primary">Sign in</button>
+      </div>
+      <div className="p-4 flex gap-2">
+        <p className="text-white/50">Don't have an account?</p>
+        <p className="text-primary">Sign up</p>
+      </div>
+    </div>
+  </div>
 );
 
 export default Auth;
