5d525155109936d2182be56a7dc684d2df1fc099
APavlov <sologod@yandex.ru>
2025-03-17 20:17:20 +0300

    reg form done

--- frontend/src/Navigation/Navigation.js
+++ frontend/src/Navigation/Navigation.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Auth from "../Auth/Auth";
 import TodoPage from "../Tasks/Tasks";
+import Registration from "../Auth/Registration";
 
 const withAuth = (Component) => {
   return (props) => {
@@ -31,6 +31,7 @@ const Navigation = () => {
       <Routes>
         <Route path="/" element={<PrivateTodoPage />} />
         <Route path="/login" element={<Auth />} />
+        <Route path="/register" element={<Registration />} />
       </Routes>
     </Router>
   );
