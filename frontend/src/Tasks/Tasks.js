2dcb585b8b7f15044600ed2ae1dd5adb41735866
APavlov <sologod@yandex.ru>
2025-03-22 14:33:58 +0300

    log out

--- frontend/src/Tasks/Tasks.js
+++ frontend/src/Tasks/Tasks.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const TodoPage = () => {
-  console.log("aaa");
+  const navigate = useNavigate();
 
-  return <div>dd</div>;
+  return (
+    <div className="h-full w-full">
+      <div
+        className="flex justify-end pr-2 pt-2 text-primary"
+        onClick={() => {
+          navigate("/login");
+          localStorage.removeItem("token");
+        }}
+      >
+        Log out
+      </div>
+    </div>
+  );
 };
 
 export default TodoPage;
